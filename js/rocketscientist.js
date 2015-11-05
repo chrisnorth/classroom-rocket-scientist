0929e00342e48c61367f03713e069ef9624e8e2a
Stuart <gravatar@strudel.org.uk>
2015-11-05 17:40:58 +0000

    Combine options. Make template system.

---
+++ js/rocketscientist.js
@@ -0,0 +1,110 @@
+// Helper functions
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+// Remove a DOM element
+function removeEl(el){
+	if(typeof el.remove==="function") el.remove(el);
+	else if(typeof el.parentElement.removeChild==="function") el.parentElement.removeChild(el);
+}
+// Check if a DOM element has the specified class
+function hasClass(el,cls){
+	if(!el) return false;
+	if(el.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))) return true;
+	else return false;
+}
+// Toggle a class on a DOM element
+function toggleClass(el,cls){
+	if(!el) return;
+	// Remove/add it
+	if(hasClass(el,cls)) el.className = el.className.replace(new RegExp("(\\s|^)" + cls + "(\\s|$)", "g")," ").replace(/ $/,'');
+	else el.className += ' '+cls;
+	return el;
+}
+var z = 1;
+function toggle3D(){
+	toggleClass(document.getElementById('satellite'),'threeD');
+}
+function zoom(factor){
+	z *= factor;
+	document.getElementById('satellite').setAttribute('style','font-size:'+z.toFixed(3)+'em');
+}
+function toggleSolar(){
+	var ids = ['sat-l','sat-m','sat-s','sat-c'];
+	for(var j = 0; j < ids.length; j++) toggleClass(document.getElementById(ids[j]),'solar-fixed');
+}
+function toggleAnimation(){
+	var ids = ['sat-l','sat-m','sat-s','sat-c'];
+	for(var j = 0; j < ids.length; j++) toggleClass(document.getElementById(ids[j]),'spin');
+}
+function setSolar(n){
+	var ids = ['sat-l','sat-m'];
+	var ul = "<ul>";
+	for(var i = 0; i < n; i++) ul += "<li class=\"solar-panel\"><\/li>";
+	ul += '<\/ul>';
+	for(var j = 0; j < ids.length; j++){
+		el = document.getElementById(ids[j]);
+		panels = el.getElementsByClassName('solar-panels');
+		for(var i = 0; i < panels.length; i++){
+			panels[i].innerHTML = ul;
+		}
+	}
+}
+function addSolar(){
+	var ids = ['sat-l','sat-m'];
+	for(var j = 0; j < ids.length; j++){
+		el = document.getElementById(ids[j]);
+		panels = el.getElementsByClassName('solar-panels');
+		for(var i = 0; i < panels.length; i++){
+			ps = panels[i].getElementsByTagName('li');
+			if(ps.length < 4) panels[i].getElementsByTagName('ol')[0].innerHTML += '<li class="solar-panel"><\/li>';
+		}
+	}
+}
+function removeSolar(){
+	var ids = ['sat-l','sat-m'];
+	for(var j = 0; j < ids.length; j++){
+		el = document.getElementById(ids[j]);
+		panels = el.getElementsByClassName('solar-panels');
+		for(var i = 0; i < panels.length; i++){
+			ps = panels[i].getElementsByTagName('li');
+			if(ps.length > 0) removeEl(ps[ps.length-1]);
+		}
+	}
+}
+function toggleComms(){
+	var slots = [{'el':'sat-l','cls':'slot4x8','type':'north no-inside'},{'el':'sat-l','cls':'slot2x4','type':'south'},{'el':'sat-m','cls':'slot2x4','type':'south'},{'el':'sat-s','cls':'slot2x4','type':'south'}];
+	var el,radio,cls,i,i2;
+	for(var s = 0; s < slots.length; s++){
+		el = document.getElementById(slots[s].el)
+		els = el.getElementsByClassName(slots[s].cls);
+		if(hasClass(els[0],'radio')) removeEl(el.getElementsByClassName('hemisphere')[0]);
+		else els[0].innerHTML = '<div class="hemisphere '+slots[s].type+'"><div class="inner"><\/div><div class="dome"><\/div><\/div>'+els[0].innerHTML;
+		toggleClass(els[0],'radio');
+		toggleClass(els[0],'slot-empty');
+	}
+	var slots = [{'el':'sat-c','cls':'slot0x2'}];
+	var el,aerial,top,cls,i,i2,ant;
+	for(var s = 0; s < slots.length; s++){
+		el = document.getElementById(slots[s].el)
+		els = el.getElementsByClassName(slots[s].cls);
+		if(hasClass(els[0],'antenna')){
+			aerial = el.getElementsByClassName('aerial');
+			for(var i = aerial.length-1; i >= 0; i--) removeEl(aerial[i]);
+		}else{
+			top = els[0].getElementsByClassName('top');
+			top[0].innerHTML = "<div class=\"aerial1 aerial\"><\/div><div class=\"aerial2 aerial\"><\/div>";
+		}
+		toggleClass(els[0],'antenna');
+		toggleClass(els[0],'slot-empty');
+	}
+}
+// On load
+r(function(){
+	// Remove elements that show noscript messages
+	var ns = document.getElementsByClassName('noscriptmsg');
+	for(var i = 0; i < ns.length;i++) removeEl(ns[i]);
+
+	// Remove classes from script only elements
+	el = document.getElementsByClassName('scriptonly');
+	for(var i = el.length-1; i >= 0;i--) toggleClass(el[i],'scriptonly');
+	
+})
---
+++ templates/template.pl
@@ -0,0 +1,128 @@
+#!/usr/bin/perl
+
+
+
+$basedir = "";
+if($0 =~ /^(.*\/)[^\/]+/){
+	$basedir = $1;
+}
+
+
+#################################
+# Process command line arguments
+#################################
+for($i = 0; $i < (@ARGV) ; $i++){
+	if($ARGV[$i] =~ /^\-+(.*)/){
+		$flag = $1;
+		if($flag eq "f" || $flag eq "file"){ $file_i = $ARGV[$i+1]; }
+		if($flag eq "o" || $flag eq "ofile"){ $file_o = $ARGV[$i+1]; }
+		if($flag eq "l" || $flag eq "lang"){ $lang = $ARGV[$i+1]; }
+		$i++;
+	}else{
+		if(!$file_i){
+			$file_i = $ARGV[$i];
+		}else{
+			if(!$file_o){
+				$file_o = $ARGV[$i];
+			}
+		}
+	}
+}
+
+
+if($ARGV[0] eq "--help" || $ARGV[0] eq "-h" || !-e $file_i || !$file_i || ($file_i eq $file_o)){
+	print <<END
+
+Stuart's template building kit
+==============================
+
+Allows HTML templates to be built in modules using SSI include commands.
+
+    -f = the input file
+    -l = the column number to sort by
+
+The results are sent to the command line so use you can pipe it into a
+file if you wish.
+
+Examples
+--------
+1. Build a template.html file into a new HTML file:
+    perl template.pl template.html 
+1. Build a template.html file into a new HTML file and save to output.html:
+    perl template.pl template.html output.html
+
+END
+}
+
+if(!$file_i){
+	print "You need to provide a template file.\n";
+	exit;
+}
+if($file_i eq $file_o){
+	print "The template file and output file can't be the same.\n";
+	exit;
+}
+if(!-e $file_i){
+	print "$file_i doesn't exist! Quitting.\n";
+	exit;
+}
+
+$result = parseFile($file_i);
+
+if($file_o){
+	$save = 0;
+	if(-e $file_o){
+		print "Overwrite $file_o (y/n)? ";
+		$a = <STDIN>;
+		$a =~ s/[\n\r]//g;
+		if($a eq "y" || $a eq "Y"){
+			$save = 1;
+		}else{
+			$save = 0;
+		}
+	}else{
+		$save = 1;
+	}
+	if($save==1){
+		open(FILE,">",$file_o);
+		print FILE $result;
+		close(FILE);
+	}else{
+		print "Quitting. Nothing written.\n";
+		exit;
+	}
+}else{
+	print "$result";
+}
+
+
+sub parseFile {
+	my ($file,$line,@lines,$indent,$html,$dir);
+	$html = "";
+	$file = $_[0];
+	$indent = ($_[1] ? $_[1] : "");
+
+	# Get the base directory for this file - to use for relative paths in any includes
+	$dir = "";
+	if($file =~ /^(.*\/)[^\/]+/){ $dir = $1; }
+
+	print "Reading $file\n";
+	open(FILE,$file);
+	@lines = <FILE>;
+	close(FILE);
+
+	foreach $line (@lines){
+		while($line =~ /^([\t\s]*)\<\!\-\- *\#include *file=\"([^\"]+)\" *\-\-\>/){
+			$ind = $indent.$1;
+			$inc = $2;
+			$insert = parseFile($dir.$inc,$ind);
+			#print "$insert\n";
+			$line =~ s/^([\t\s]*)\<\!\-\-\#include file=\"$inc\" \-\-\>/$insert/;
+		}
+		# Update any language variables
+		#while(<!--#lang var="title" -->){
+		#}
+		$html .= "$indent$line";
+	}
+	return $html;
+}
