8c6739493901bc4f8dbbcb6246d889facc06f64c
Chris North <chris.north@astro.cf.ac.uk>
2016-12-09 14:07:51 +0000

    added } & some ;

--- js/rocketscientist.js
+++ js/rocketscientist.js
@@ -143,15 +143,16 @@ var rs;
 			if(location.protocol==="file:"){
 				var el = S('#start a');
 				for(var i = 0; i < el.length; i++){
-					if (el[i].href.substring(el[i].href.length-4)==="html"){el[i].href=el[i].href}
+					if (el[i].href.substring(el[i].href.length-4)==="html"){el[i].href=el[i].href;}
 					else{el[i].href = el[i].href+".html";}
 					// el[i].href = el[i].href +  (el[i].href.substring((el[i].href.length)-4)==="html") ? "" : ".html";
 				}
 			}else{
 				var el = S('#start a');
 				for(var i = 0; i < el.length; i++){
-					if (el[i].href.substring(el[i].href.length-4)==="html"){el[i].href=el[i].href}
+					if (el[i].href.substring(el[i].href.length-4)==="html"){el[i].href=el[i].href;}
 					else{el[i].href = el[i].href+".html";}
+				}
 			}
 			return this;
 		}
